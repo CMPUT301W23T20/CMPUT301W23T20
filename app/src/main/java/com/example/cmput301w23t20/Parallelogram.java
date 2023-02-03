1c6e50afbe8af2a40cd4c8a4680db5e9a1bbf19e
Rex <yongxin1@ualberta.ca>
2023-02-02 20:23:49 -0700

    Parallelogram-rex

--- app/src/main/java/com/example/cmput301w23t20/Parallelogram.java
+++ app/src/main/java/com/example/cmput301w23t20/Parallelogram.java
@@ -1,2 +1,7 @@
-package com.example.cmput301w23t20;public class Parallelogram {
+package com.example.cmput301w23t20;
+
+public class Parallelogram extends Shape{
+    public Parallelogram(int x, int y) {
+    }
 }
+
