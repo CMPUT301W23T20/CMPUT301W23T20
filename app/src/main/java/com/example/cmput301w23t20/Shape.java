d17106f3d8dbc0ed654a3d3cd0bcdc39389621fb
Muhanned Al-Bachachi <momo.albachachi@gmail.com>
2023-02-02 20:20:59 -0700

    float

--- app/src/main/java/com/example/cmput301w23t20/Shape.java
+++ app/src/main/java/com/example/cmput301w23t20/Shape.java
@@ -2,5 +2,5 @@ package com.example.cmput301w23t20;
 public abstract class Shape {
     private int x;
     private int y;
-    String color = "$PUT_YOUR_CHOICE_OF_COLOR";
+    float color = 10.0F;
 }
